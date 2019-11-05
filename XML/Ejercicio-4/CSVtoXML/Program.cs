fb83a75531f84d45749bca0f946d0d6395689764
Isabel Fernández Pérez <43760042+isafdezpe@users.noreply.github.com>
2019-11-05 13:45:44 +0100

    ej4

---
+++ XML/Ejercicio-4/CSVtoXML/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using System.Xml.Linq;
+using System.IO;
+
+namespace CSVtoXML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 2)
+            {
+                Console.WriteLine("Deben especificarse dos archivos como parámetro");
+                return;
+            }
+            if (args[0] == null)
+            {
+                Console.WriteLine("No se ha especificado archivo de entrada");
+                return;
+            }
+
+            if (!args[0].EndsWith(".csv"))
+            {
+                Console.WriteLine("El archivo de entrada especificado no es un CSV");
+                return;
+            }
+
+            if (args[1] == null)
+            {
+                Console.WriteLine("No se ha especificado archivo de salida");
+                return;
+            }
+
+            var csvContent = File.ReadAllLines(args[0]);
+
+            var name = args[0].Split('.')[0];
+            var xmlContent = new XElement(name,
+                csvContent.Select(line => new XElement("Item",
+                    line.Split(';')
+                        .Select((column, index) => new XElement("Column" + index, column)))));
+
+            File.WriteAllText(args[1] + ".xml", xmlContent.ToString());
+        }
+    }
+}
---
+++ XML/Ejercicio-4/CSVtoXML/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada a un ensamblado.
+[assembly: AssemblyTitle("CSVtoXML")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CSVtoXML")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("7e981f51-4f6c-4b57-b803-5cdbe6c2cd79")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+// Puede especificar todos los valores o usar los valores predeterminados de número de compilación y de revisión
+// utilizando el carácter "*", como se muestra a continuación:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
