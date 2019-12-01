43ab88200348465d6b394227fd4ed816e80e4bce
isafdezpe <isafdezpe98@gmail.com>
2019-12-01 19:42:59 +0100

    ej14-1 para arreglar

--- Javascript/Ejercicio14/Tarea-1/MapaKML.js
+++ Javascript/Ejercicio14/Tarea-1/MapaKML.js
@@ -3,16 +3,44 @@ class MapaKML {
     initMap() {
         var mapOptions = {
             zoom: 7,
-            center: new google.maps.LatLng(40.4167, -3.70325),
+            center: new google.maps.LatLng(43.370027,-5.8835068),
             mapTypeId: "terrain"
         };
-        var map = new google.maps.Map(document.getElementById("mapa"), mapOptions);
-        
-        var kmlLayer = new google.maps.KmlLayer("rutas.kml", {
-            suppressInfoWindows: true,
-            preserveViewport: false,
-            map: map
+        this.map = new google.maps.Map(document.getElementById("mapa"), mapOptions);
+    }
+
+    cargarKml(files) {
+        var archivo = files[0];
+        var tipo = "application/vnd.google-earth.kml+xml";
+
+        var parser = new geoXML3.parser({
+            map: this.map,
+            singleInfoWindow: true,
+            afterParse: this.useTheData
         });
+
+        if (archivo.type == tipo) {
+            var reader = new FileReader();
+            reader.onload = function (event) {
+                var text = reader.result;
+
+                parser.fetchXML = function (url, callback) {
+                    function timeoutHandler() {
+                        callback();
+                    };
+                    $.ajax({
+                        type: "GET",
+                        url: url,
+                        success: function (xml) {
+                            callback(xml);
+                        }
+                    });
+                };
+
+                parser.parseKmlString(text);
+            }
+            lector.readAsText(archivo);
+        }
     }
 
 }
